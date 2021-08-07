939ae8ba123789ddd5942a8698a403560285f3ed
Justin <oriontech15@gmail.com>
2021-08-06 20:17:05 -0600

    Setup initial VC and Model for project.

---
+++ ExtraSpaceChallenge/Model/PhotoModel.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoModel.swift
+//  ExtraSpaceChallenge
+//
+//  Created by orion on 8/6/21.
+//
+
+import Foundation
+
+struct PhotoModel: Codable {
+    
+    var albumId: String
+    var id: String
+    var title: String
+    var url: String
+    var thumbnailUrl: String
+    
+}
--- ExtraSpaceChallenge/ViewController.swift
+++ ExtraSpaceChallenge/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+}
 
-
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor.init(red: CGFloat.random(in: 0..<1.0), green: CGFloat.random(in: 0..<1.0), blue: CGFloat.random(in: 0..<1.0), alpha: 1.0)
+        
+        return cell
+    }
 }
 
